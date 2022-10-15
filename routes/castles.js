a57017cd5566755ab44f8c72115589c3e3c53601
OneDeMillion <knittinbrit@gmail.com>
2022-10-15 14:27:54 -0500

    rest clients created for all local host

--- routes/castles.js
+++ routes/castles.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('A castle to visit');
-});
+const castlesController = require('../controllers/castles');
+
+// gets all castles in collection
+router.get('/', castlesController.getAll);
+
+// gets one castle in collection by id
+router.get('/:id', castlesController.getOne);
+
+// creates a new castle document
+router.post('/', castlesController.createOne);
+
+// updates a castle document identified by id
+router.post('/:id', castlesController.updateOne);
+
+// deletes a castle document identified by id
+router.delete('/:id', castlesController.deleteOne);
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const mongodb = require('./initializers/db'); 
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 8080;
 
 //middlewares -- function that executes when a specific route is being hit
 app
     .use(bodyParser.json())
     .use((req, res, next) => {
-        //res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*');
         next();
     })
     .use('/', require('./routes'));
 
 
-
-//routes
-app.get('/', (req, res) => {
-    res.send('A place to visit');
-});
 
 mongodb.initDb((err) => {
     if (err) {
