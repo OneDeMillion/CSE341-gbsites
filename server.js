b3323d08438ed48b24189dbfe2f31407fbea9805
OneDeMillion <knittinbrit@gmail.com>
2022-10-29 13:09:30 -0500

    auth0 login and logout added to app

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const { auth, requiresAuth } = require('express-openid-connect');
 const mongodb = require('./initializers/db'); 
 const port = process.env.PORT || 8080;
 //const swaggerAutogen = require('swagger-autogen')();
+
+const config = {
+  authRequired: true,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
 
 //middlewares -- function that executes when a specific route is being hit
 app
